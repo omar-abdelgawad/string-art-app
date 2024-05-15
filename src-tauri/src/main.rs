a20b734b9da27aaf45303afd3b3a0ec0cb7289ae
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2024-05-15 13:42:37 +0300

    wrote initial layout of backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,7 +9,37 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(AppState {
+            string_ring: Mutex::new(StringRing::new()),
+        })
+        .invoke_handler(tauri::generate_handler![greet, insert_data])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+//////////
+
+use std::sync::Mutex;
+
+struct AppState {
+    string_ring: Mutex<StringRing>,
+}
+
+//////////////////////////
+mod utils;
+use utils::{str_to_image, StringRing};
+#[tauri::command]
+fn insert_data(
+    data: &str,
+    width: i32,
+    height: i32,
+    num_nails: i32,
+    opacity: f64,
+    state: tauri::State<AppState>,
+) {
+    assert_eq!(width, height);
+    assert!(num_nails > 0);
+    assert!(opacity >= 0.0 && opacity <= 1.0);
+    let image = str_to_image(data, width, height);
+    let mut stringring = state.string_ring.lock().unwrap();
+    stringring.set_parameters(width / 2, height / 2, width / 2, num_nails, image, opacity);
 }
