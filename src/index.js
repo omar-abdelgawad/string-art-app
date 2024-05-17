3434c40d86d67f28720b171eccaf64cdcd12c06c
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2024-05-17 19:16:48 +0300

    removed unnecessary code , html elements and formatted some files

--- src/index.js
+++ src/index.js
@@ -7,138 +7,87 @@ import { drawImage } from './drawImage.js';
 // import { uploading } from './uploading.js';
 
 
-// form submit function
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('stringArtForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    generateStringArt();
-  });
-});
 
-window.onload = function() {
+
+window.onload = function () {
   let canvas = document.getElementById('imageCanvas');
   let ctx = canvas.getContext('2d');
 
   initializeImageUpload(canvas, ctx, drawImage);
 }
-// Update image function
-// document.getElementById('updateButton').addEventListener('click', function () {
-//   const img = new Image(); // Create a new Image object
-//   img.src = '/test_files/neko.jpg'; // Set the source of the image to the local file path
-//   img.onload = function () { // Define the onload event handler for when the image is loaded
-//     const canvas = document.getElementById('imageCanvas'); // Get the image canvas element
-//     const ctx = canvas.getContext('2d'); // Get the 2D rendering context of the image canvas
-//     canvas.width = img.width; // Set the width of the image canvas to the width of the image
-//     canvas.height = img.height; // Set the height of the image canvas to the height of the image
-//     ctx.drawImage(img, 0, 0); // Draw the image onto the image canvas
-//   };
-// });
 
-// upload image function
-document.getElementById('updateButton').addEventListener('click', async function (event) { 
-    const img = new Image(); 
-    img.src = document.getElementById('imageCanvas').toDataURL(); 
-    img.onload = function () { 
-      const canvas_tmp = document.createElement('canvas'); 
-      const ctx_tmp = canvas_tmp.getContext('2d'); 
-      canvas_tmp.width = 900; 
-      canvas_tmp.height = 900; 
-      ctx_tmp.drawImage(img, 0, 0, canvas_tmp.width, canvas_tmp.height);
-      
+document.getElementById('updateButton').addEventListener('click', async function (event) {
+  const img = new Image();
+  img.src = document.getElementById('imageCanvas').toDataURL();
+  img.onload = function () {
+    const canvas_tmp = document.createElement('canvas');
+    const ctx_tmp = canvas_tmp.getContext('2d');
+    canvas_tmp.width = 900;
+    canvas_tmp.height = 900;
+    ctx_tmp.drawImage(img, 0, 0, canvas_tmp.width, canvas_tmp.height);
 
-      const imageData = ctx_tmp.getImageData(0, 0, canvas_tmp.width, canvas_tmp.height); 
-      const data = imageData.data; 
-      let grayImageAsSpaceSeparatedString = ""; 
 
-      for (let i = 0; i < data.length; i += 4) { 
-        const r = data[i]; 
-        const g = data[i + 1]; 
-        const b = data[i + 2]; 
-        const grayscale = Math.floor((r + g + b) / 3); 
-        data[i] = data[i + 1] = data[i + 2] = grayscale; 
-        grayImageAsSpaceSeparatedString += grayscale + " "; 
+    const imageData = ctx_tmp.getImageData(0, 0, canvas_tmp.width, canvas_tmp.height);
+    const data = imageData.data;
+    let grayImageAsSpaceSeparatedString = "";
+
+    for (let i = 0; i < data.length; i += 4) {
+      const r = data[i];
+      const g = data[i + 1];
+      const b = data[i + 2];
+      const grayscale = Math.floor((r + g + b) / 3);
+      data[i] = data[i + 1] = data[i + 2] = grayscale;
+      grayImageAsSpaceSeparatedString += grayscale + " ";
+    }
+    ctx_tmp.putImageData(imageData, 0, 0);
+    const grayInputImagePreview = document.getElementById('grayImageInputPreview');
+    grayInputImagePreview.src = canvas_tmp.toDataURL();
+    grayImageAsSpaceSeparatedString = grayImageAsSpaceSeparatedString.trim();
+    const { canvas, ctx } = getCanvasContext();
+    const radius = canvas.width / 2;
+    const numNails = parseInt(document.getElementById('numNailsInput').value, 10);
+    const nails = getNailPositions(numNails, canvas, radius);
+    for (let i = 0; i < numNails; i++) {
+      const nail = nails[i];
+      ctx.beginPath();
+      ctx.arc(nail.x, nail.y, 1, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+    console.log(grayImageAsSpaceSeparatedString, canvas_tmp.width, canvas_tmp.height, numNails);
+    invoke('insert_data', {
+      data: grayImageAsSpaceSeparatedString,
+      width: canvas_tmp.width,
+      height: canvas_tmp.height,
+      numNails: numNails,
+      opacity: 0.3,
+    });
+    const numIterations = 2000;
+    let cur_nail = 0;
+    let delayInMilliseconds = 5;
+    async function performLoop() {
+      for (let i = 0; i < numIterations; i++) {
+        const nail = await invoke('get_next_nail');
+        const startNail = nails[cur_nail];
+        const endNail = nails[nail % numNails];
+        ctx.strokeStyle = 'rgba(87, 87, 87,0.2)';
+        ctx.beginPath();
+        ctx.moveTo(startNail.x, startNail.y);
+        ctx.lineTo(endNail.x, endNail.y);
+        ctx.stroke();
+        cur_nail = nail;
+        await new Promise(resolve => setTimeout(resolve, delayInMilliseconds));
       }
-      ctx_tmp.putImageData(imageData, 0, 0); 
-      const grayInputImagePreview = document.getElementById('grayImageInputPreview'); 
-      grayInputImagePreview.src = canvas_tmp.toDataURL(); 
-      grayImageAsSpaceSeparatedString = grayImageAsSpaceSeparatedString.trim(); 
-      const { canvas, ctx } = getCanvasContext(); 
-      const radius = canvas.width / 2; 
-      const numNails = parseInt(document.getElementById('numNailsInput').value, 10); 
-      const nails = getNailPositions(numNails, canvas, radius); 
-      for (let i = 0; i < numNails; i++) { 
-        const nail = nails[i]; 
-        ctx.beginPath(); 
-        ctx.arc(nail.x, nail.y, 1, 0, 2 * Math.PI); 
-        ctx.fill(); 
-      }
-      console.log(grayImageAsSpaceSeparatedString,canvas_tmp.width,canvas_tmp.height,numNails);
-      invoke('insert_data', { 
-        data: grayImageAsSpaceSeparatedString, 
-        width: canvas_tmp.width, 
-        height: canvas_tmp.height, 
-        numNails: numNails, 
-        opacity: 0.3, 
-      });
-      const numIterations = 2000; 
-      let cur_nail = 0; 
-      let delayInMilliseconds = 5; 
-      async function performLoop() { 
-        for (let i = 0; i < numIterations; i++) { 
-          const nail = await invoke('get_next_nail'); 
-          const startNail = nails[cur_nail]; 
-          const endNail = nails[nail % numNails]; 
-          ctx.strokeStyle = 'rgba(87, 87, 87,0.2)'; 
-          ctx.beginPath(); 
-          ctx.moveTo(startNail.x, startNail.y); 
-          ctx.lineTo(endNail.x, endNail.y); 
-          ctx.stroke(); 
-          cur_nail = nail; 
-          await new Promise(resolve => setTimeout(resolve, delayInMilliseconds)); 
-        }
-      }
-      performLoop(); 
-    };
+    }
+    performLoop();
+  };
 });
 
-function generateStringArt() {
-  console.log("Generating string art...");                        // Log to indicate the start of the function
-  const { canvas, ctx } = getCanvasContext();
-  const radius = canvas.width / 2; // Define radius here
-  if (!ctx) {
-    return;
-  }
 
 
-  const numNailsInput = document.getElementById('numNailsInput'); // Get the input element for number of nails
-  const numNails = parseInt(numNailsInput.value, 10);             // Parse the number of nails from the input value
-  if (isNaN(numNails)) {                                          // Check if the parsed number is not a number
-    console.error('Invalid number of nails!');                  // Log an error message if invalid
-    return;
-  }
-
-  const nails = getNailPositions(numNails, canvas, radius);
-  for (let i = 0; i < numNails; i++) {
-    const nail = nails[i];
-    ctx.beginPath();
-    ctx.arc(nail.x, nail.y, 1, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-
-  const nailConnectionsInput = document.getElementById('nailConnections'); // Add this line
-
-  const nailSequence = nailConnectionsInput.value.split(',').map(n => parseInt(n, 10));
-
-  console.log("Nail positions:", nails);                         // Log the nails object to the console for debugging
-  var delayInMilliseconds = 50; //1 second
-  // Connect specified nail pairs using the object keys
-  drawLinesBetweenNails(nails, nailSequence, ctx, delayInMilliseconds);
-}
-
-window.downloadImage = function() {
+window.downloadImage = function () {
   var download = document.getElementById("download");
   var image = document.getElementById("stringArtCanvas").toDataURL("image/png")
-      .replace("image/png", "image/octet-stream");
+    .replace("image/png", "image/octet-stream");
   download.setAttribute("href", image);
   console.log("download")
 }
--- src/loadImage.js
+++ src/loadImage.js
@@ -4,10 +4,10 @@ let zoomSlider = document.getElementById('zoomSlider');
 let rotateSlider = document.getElementById('rotateSlider');
 
 export function initializeImageUpload(canvas, ctx, drawImage) {
-    document.getElementById('imageUpload').addEventListener('change', function(e) {
+    document.getElementById('imageUpload').addEventListener('change', function (e) {
         let reader = new FileReader();
-        reader.onload = function(event) {
-            img.onload = function() {
+        reader.onload = function (event) {
+            img.onload = function () {
                 drawImage(img, canvas, ctx, zoomSlider.value, rotateSlider.value);
             }
             img.src = event.target.result;
@@ -15,11 +15,11 @@ export function initializeImageUpload(canvas, ctx, drawImage) {
         reader.readAsDataURL(e.target.files[0]);
     });
 
-    zoomSlider.addEventListener('input', function() {
+    zoomSlider.addEventListener('input', function () {
         drawImage(img, canvas, ctx, zoomSlider.value, rotateSlider.value);
     });
 
-    rotateSlider.addEventListener('input', function() {
+    rotateSlider.addEventListener('input', function () {
         drawImage(img, canvas, ctx, zoomSlider.value, rotateSlider.value);
     });
 }
