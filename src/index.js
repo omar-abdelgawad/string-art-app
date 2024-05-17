4c5048bf70d7faa4e79bc79ea6eaadaebd8a3836
Khaled Gad <khaled.gad@ejust.edu.eg>
2024-05-17 18:28:00 +0300

    pipeline kinda works-weired dimensions do not work

--- src/index.js
+++ src/index.js
@@ -21,95 +21,87 @@ window.onload = function() {
 
   initializeImageUpload(canvas, ctx, drawImage);
 }
-document.getElementById('updateButton').addEventListener('click', function() {
-  var canvas = document.getElementById('imageCanvas');
-  var image = canvas.toDataURL('image/png');
-  var link = document.createElement('a');
-  link.download = 'edited_image.png';
-  link.href = image;
-  link.click();
-});
+// Update image function
+// document.getElementById('updateButton').addEventListener('click', function () {
+//   const img = new Image(); // Create a new Image object
+//   img.src = '/test_files/neko.jpg'; // Set the source of the image to the local file path
+//   img.onload = function () { // Define the onload event handler for when the image is loaded
+//     const canvas = document.getElementById('imageCanvas'); // Get the image canvas element
+//     const ctx = canvas.getContext('2d'); // Get the 2D rendering context of the image canvas
+//     canvas.width = img.width; // Set the width of the image canvas to the width of the image
+//     canvas.height = img.height; // Set the height of the image canvas to the height of the image
+//     ctx.drawImage(img, 0, 0); // Draw the image onto the image canvas
+//   };
+// });
 
 
 // upload image function
-document.getElementById('imageInput').addEventListener('change', async function (event) {
-  const file = event.target.files[0];
-  const reader = new FileReader();
-  reader.onload = function (e) {
-    // first resize the image to (900,900)
-    const inputImagePreview = document.getElementById('imageInputPreview');
-    inputImagePreview.src = e.target.result;
-    // Load the image into a canvas to convert it to grayscale
-    const img = new Image();
-    img.src = e.target.result;
-    img.onload = function () {
-      const canvas_tmp = document.createElement('canvas');
-      const ctx_tmp = canvas_tmp.getContext('2d');
-      canvas_tmp.width = img.width;
-      canvas_tmp.height = img.height;
-      ctx_tmp.drawImage(img, 0, 0);
+document.getElementById('updateButton').addEventListener('click', async function (event) { 
+    const img = new Image(); 
+    img.src = document.getElementById('imageCanvas').toDataURL(); 
+    img.onload = function () { 
+      const canvas_tmp = document.createElement('canvas'); 
+      const ctx_tmp = canvas_tmp.getContext('2d'); 
+      canvas_tmp.width = 900; 
+      canvas_tmp.height = 900; 
+      ctx_tmp.drawImage(img, 0, 0, canvas_tmp.width, canvas_tmp.height);
+      
 
-      const imageData = ctx_tmp.getImageData(0, 0, canvas_tmp.width, canvas_tmp.height);
-      const data = imageData.data;
-      let grayImageAsSpaceSeparatedString = "";
+      const imageData = ctx_tmp.getImageData(0, 0, canvas_tmp.width, canvas_tmp.height); 
+      const data = imageData.data; 
+      let grayImageAsSpaceSeparatedString = ""; 
 
-      // Convert each pixel to grayscale
-      for (let i = 0; i < data.length; i += 4) {
-        const r = data[i];
-        const g = data[i + 1];
-        const b = data[i + 2];
-        const grayscale = Math.floor((r + g + b) / 3);
-        data[i] = data[i + 1] = data[i + 2] = grayscale;
-        grayImageAsSpaceSeparatedString += grayscale + " ";
+      for (let i = 0; i < data.length; i += 4) { 
+        const r = data[i]; 
+        const g = data[i + 1]; 
+        const b = data[i + 2]; 
+        const grayscale = Math.floor((r + g + b) / 3); 
+        data[i] = data[i + 1] = data[i + 2] = grayscale; 
+        grayImageAsSpaceSeparatedString += grayscale + " "; 
       }
-      ctx_tmp.putImageData(imageData, 0, 0);
-
-      // Set the grayscale image source to the grayscale image preview
-      const grayInputImagePreview = document.getElementById('grayImageInputPreview');
-      grayInputImagePreview.src = canvas_tmp.toDataURL();
-      ///////////////////////////////////////////////////////////////////////
-
-      grayImageAsSpaceSeparatedString = grayImageAsSpaceSeparatedString.trim();
-
-      const { canvas, ctx } = getCanvasContext();
-      const radius = canvas.width / 2;
-      const numNails = parseInt(document.getElementById('numNailsInput').value, 10);
-      const nails = getNailPositions(numNails, canvas, radius);
-      for (let i = 0; i < numNails; i++) {
-        const nail = nails[i];
-        ctx.beginPath();
-        ctx.arc(nail.x, nail.y, 1, 0, 2 * Math.PI);
-        ctx.fill();
+      ctx_tmp.putImageData(imageData, 0, 0); 
+      const grayInputImagePreview = document.getElementById('grayImageInputPreview'); 
+      grayInputImagePreview.src = canvas_tmp.toDataURL(); 
+      grayImageAsSpaceSeparatedString = grayImageAsSpaceSeparatedString.trim(); 
+      const { canvas, ctx } = getCanvasContext(); 
+      const radius = canvas.width / 2; 
+      const numNails = parseInt(document.getElementById('numNailsInput').value, 10); 
+      const nails = getNailPositions(numNails, canvas, radius); 
+      for (let i = 0; i < numNails; i++) { 
+        const nail = nails[i]; 
+        ctx.beginPath(); 
+        ctx.arc(nail.x, nail.y, 1, 0, 2 * Math.PI); 
+        ctx.fill(); 
       }
-      invoke('insert_data', {
-        data: grayImageAsSpaceSeparatedString,
-        width: canvas_tmp.width,
-        height: canvas_tmp.height,
-        numNails: numNails,
-        opacity: 0.3,
+      console.log(grayImageAsSpaceSeparatedString,canvas_tmp.width,canvas_tmp.height,numNails);
+      invoke('insert_data', { 
+        data: grayImageAsSpaceSeparatedString, 
+        width: canvas_tmp.width, 
+        height: canvas_tmp.height, 
+        numNails: numNails, 
+        opacity: 0.3, 
       });
-      const numIterations = 2000;
-      let cur_nail = 0;
-      let delayInMilliseconds = 5;
-      async function performLoop() {
-        for (let i = 0; i < numIterations; i++) {
-          const nail = await invoke('get_next_nail');
-          const startNail = nails[cur_nail];
-          const endNail = nails[nail % numNails];
-          ctx.strokeStyle = 'rgba(87, 87, 87,0.2)';
-          ctx.beginPath();
-          ctx.moveTo(startNail.x, startNail.y);
-          ctx.lineTo(endNail.x, endNail.y);
-          ctx.stroke();
-          cur_nail = nail;
-          await new Promise(resolve => setTimeout(resolve, delayInMilliseconds));
+      const numIterations = 2000; 
+      let cur_nail = 0; 
+      let delayInMilliseconds = 5; 
+      async function performLoop() { 
+        for (let i = 0; i < numIterations; i++) { 
+          const nail = await invoke('get_next_nail'); 
+          const startNail = nails[cur_nail]; 
+          const endNail = nails[nail % numNails]; 
+          ctx.strokeStyle = 'rgba(87, 87, 87,0.2)'; 
+          ctx.beginPath(); 
+          ctx.moveTo(startNail.x, startNail.y); 
+          ctx.lineTo(endNail.x, endNail.y); 
+          ctx.stroke(); 
+          cur_nail = nail; 
+          await new Promise(resolve => setTimeout(resolve, delayInMilliseconds)); 
         }
       }
-      performLoop();
+      performLoop(); 
     };
-  };
-  reader.readAsDataURL(file);
 });
+
 
 function generateStringArt() {
   console.log("Generating string art...");                        // Log to indicate the start of the function
